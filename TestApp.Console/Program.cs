78aa8ea8c2ec6e8a053e29cda1d290179f34b6a6
reailistuz <reailistuz@gmail.com>
2023-09-11 12:19:20 +0500

    #1 - added project

---
+++ TestApp.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestApp.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            var i = 10;
+            i++;
+            System.Console.WriteLine(i);
+
+
+        }
+
+        public static int Sum(int addendOne, int addendTwo)
+        {
+
+            return addendOne + addendTwo;
+
+        }
+
+
+    }
+}
---
+++ TestApp.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+namespace TestApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void TestFail()
+        {
+
+            Assert.True(true, "True is not false");
+
+        }
+
+        [Fact]
+        public void TestSum()
+        {
+
+            Assert.Equal(4, TestApp.Console.Program.Sum(2, 2));
+
+        }
+
+        [Fact]
+        public void TestSum2()
+        {
+            Assert.Equal(5, TestApp.Console.Program.Sum(2, 3));
+        }
+
+
+    }
+}
